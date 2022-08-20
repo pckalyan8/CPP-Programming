d9e69d2fcba9eb1e1c13e7ce2a541a2e45ef0ec5
Kalyan_Prathapaneni <Kalyan.Prathapaneni@skillassure.com>
2022-08-20 15:10:37 +0530

    dynamic multi dimentional array fix

--- Programs/Learning CPP/07.Pointers/mulit-dimentional dynamic arrays.cpp
+++ Programs/Learning CPP/07.Pointers/mulit-dimentional dynamic arrays.cpp
@@ -8,8 +8,9 @@ int main()
 {
     // multi dimensional dynamic array
     int rows, cols;
-    cout << "Enter rows and cols: ";
-    cin >> rows >> cols;
+    cout << "Enter rows: "  << endl;
+    cin >> rows;
+    cols = rows;
 
     int **table = new int *[rows];
     for (int i = 0; i < cols; i++)
@@ -19,6 +20,17 @@ int main()
 
     // assign value to first row and first column
     table[0][0] = 1000;
+    cout << "Value of first row and first column: " << table[0][0] << endl;
+
+    // display the table
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     for (int i = 0; i < rows; i++)
     {
